ed67e1e03faff3ca27a94ab8b2c0317e846d4a5e
BigNerd95 <lorenzo.santina@edu.unito.it>
2016-12-27 02:25:48 +0100

    Added comments

--- Examples/carrier_generator.c
+++ Examples/carrier_generator.c
@@ -1,4 +1,9 @@
 /*
+Goal:
+    This example is for educational purposes only, and is fully commented
+    This example only generate a carrier on passed frequency or on default frequency 104.5 MHz
+    Turn your car radio on and check if there is silence on that frequency
+
 Usage:
     Compile:
         gcc carrier_generator.c -o carrier_generator -L /opt/vc/lib -l bcm_host -I /opt/vc/include
@@ -20,11 +25,12 @@ References:
             - 108 (Clock Manager General Purpose Clock Divisors)
 
     https://github.com/raspberrypi/userland/blob/master/host_applications/linux/libs/bcm_host/bcm_host.c
-        Functions:
+        Functions to allow compatibility on RPi 1 and successors:
             - unsigned bcm_host_get_peripheral_address(void)
             - unsigned bcm_host_get_peripheral_size(void)
             - unsigned bcm_host_get_sdram_address(void)
 
+    https://pinout.xyz/pinout/gpclk
     https://pinout.xyz/pinout/pin7_gpio4
 
 License:
@@ -34,35 +40,28 @@ Author:
     Lorenzo Santina (BigNerd95)
 */
 
-#include <bcm_host.h>   // Broadcom library to support any version of RaspBerry
-#include <stdio.h>      // Input/Output functions
-#include <stdint.h>     // Integer types having specified widths
-#include <fcntl.h>      // Function open
-#include <sys/types.h>  // Sometimes needed by fcntl
-#include <sys/stat.h>   // Sometimes needed by fcntl
-#include <sys/mman.h>   // Function mmap
+#include <bcm_host.h>           // Broadcom library to support any version of RaspBerry
+#include <stdio.h>              // Input/Output functions
+#include <stdint.h>             // Integer types having specified widths
+#include <fcntl.h>              // Function open
+#include <sys/types.h>          // Sometimes needed by fcntl
+#include <sys/stat.h>           // Sometimes needed by fcntl
+#include <sys/mman.h>           // Function mmap
+
 
 // Physical peripheral base addresses
 #define BASE_ADDRESS 0x7E000000
-// Clock Manager General Purpose Clocks Control registers
-#define CM_GP0CTL 0x7E101070    // GPCLK0
-#define CM_GP1CTL 0x7E101078    // not used
-#define CM_GP2CTL 0x7E101080    // not used
 
-// Clock Manager General Purpose Clock Divisors registers
-#define CM_GP0DIV 0x7E101074    // GPCLK0
-#define CM_GP1DIV 0x7E10107c    // not used
-#define CM_GP2DIV 0x7E101084    // not used
 
 // General Purpose I/O function select registers
-#define GPFSEL0  0x7E200000     // BCM pin 0 to 9, we use 4
-#define GPFSEL1  0x7E200004     // BCM pin 10 to 19 (register not used)
-#define GPFSEL2  0x7E200008     // BCM pin 20 to 29 (register not used)
-#define GPFSEL3  0x7E20000C     // BCM pin 30 to 39 (register not used)
-#define GPFSEL4  0x7E200010     // BCM pin 40 to 49 (register not used)
-#define GPFSEL5  0x7E200014     // BCM pin 50 to 53 (register not used)
+#define GPFSEL0  0x7E200000     // BCM pin 0 to 9, we use 4 [GPIO4 pin 7]
+#define GPFSEL1  0x7E200004     // BCM pin 10 to 19 (register not used in this example)
+#define GPFSEL2  0x7E200008     // BCM pin 20 to 29 (register not used in this example)
+#define GPFSEL3  0x7E20000C     // BCM pin 30 to 39 (register not used in this example)
+#define GPFSEL4  0x7E200010     // BCM pin 40 to 49 (register not used in this example)
+#define GPFSEL5  0x7E200014     // BCM pin 50 to 53 (register not used in this example)
 #define GPFSEL {GPFSEL0,GPFSEL1,GPFSEL2,GPFSEL3,GPFSEL4,GPFSEL5} // Array structure to access faster to function registers
-#define FLAG_PER_REGISTER 10    // There are 10 pin flag per register
+#define FLAGS_PER_REGISTER 10    // There are 10 pin flags per register
 // GPIO functions flags (each flag is 3 bit long)
 typedef enum {
   GP_INPUT  = 0b000,
@@ -75,13 +74,53 @@ typedef enum {
   GP_AF5    = 0b010
 } GP_FUNCTION;
 
+
+// Clock Manager General Purpose Clocks Control registers
+#define CM_GP0CTL 0x7E101070    // GPCLK0 Control register [GPIO4 pin  7]
+#define CM_GP1CTL 0x7E101078    // GPCLK1 Control register [GPIO5 pin 29] (not used in this example)
+#define CM_GP2CTL 0x7E101080    // GPCLK2 Control register [GPIO6 pin 31] (not used in this example)
+#define CM_CTL {CM_GP0CTL,CM_GP1CTL,CM_GP2CTL}
+// Clock Manager General Purpose Clock Divisors registers
+#define CM_GP0DIV 0x7E101074    // GPCLK0 Divisor register [GPIO4 pin  7]
+#define CM_GP1DIV 0x7E10107C    // GPCLK1 Divisor register [GPIO5 pin 29] (not used in this example)
+#define CM_GP2DIV 0x7E101084    // GPCLK2 Divisor register [GPIO6 pin 31] (not used in this example)
+#define CM_DIV {CM_GP0DIV,CM_GP1DIV,CM_GP2DIV}
+// Clock Managers General Purpose Clocks
+typedef enum {
+    CM_GPCLK0 = 0,
+    CM_GPCLK1 = 1,
+    CM_GPCLK2 = 2
+} CM_GPCLK;
+// Clock Manager flags
+#define CM_PASSWD (0x5A << 24)
+#define CM_ENAB   (1 << 4)
+#define CM_BUSY   (1 << 7)
+typedef enum {
+    CM_MASH_INT = (0 << 9),
+    CM_MASH_1S  = (1 << 9),
+    CM_MASH_2S  = (2 << 9),
+    CM_MASH_3S  = (3 << 9)
+} CM_MASH;
+typedef enum {
+  CM_GND           = 0,
+  CM_OSCILLATOR    = 1,
+  CM_TESTDEBUD0    = 2,
+  CM_TESTDEBUG1    = 3,
+  CM_PLLA          = 4,
+  CM_PLLC          = 5,
+  CM_PLLD          = 6,
+  CM_HDMI          = 7
+} CM_SRC;
+
 #define PLLDFREQ     500000000. // PLLD clock source frequency (500MHz)
 
-void* MAP_ADDRESS = NULL;
+
+void* MAP_ADDRESS = NULL;       // Global variable where peripherals map address will be contained
 #define CONVERT(address)    ((unsigned) (address) - BASE_ADDRESS + MAP_ADDRESS)  // convert physical address to virtual address
 #define SET(address, value) ((*(uint32_t*) CONVERT(address)) = (value))          // set value to physical address
 #define GET(address)        (*(uint32_t*) CONVERT(address))                      // get value from physical address
 
+// Maps 'address' in the virtual address space of the process
 void* memory_map(unsigned address, size_t size) {
 
     int fd = open("/dev/mem", O_RDWR | O_SYNC);     // Open /dev/mem to map 'address'
@@ -96,16 +135,18 @@ void* memory_map(unsigned address, size_t size) {
         exit(-1);
     }
 
-    close(fd);     // Close /dev/mem fd
-    return vaddr;  // Return mapped address
+    close(fd);                                      // Close /dev/mem fd
+    return vaddr;                                   // Return mapped address
 }
 
+// Maps the correct peripheral address in the virtual address space of the process
 void map_peripheral(){
-    unsigned peripheral_address = bcm_host_get_peripheral_address();  // Broadcom function to get peripheral address (so it support any RaspBerry version)
+    unsigned peripheral_address = bcm_host_get_peripheral_address();  // Broadcom function to get peripheral address (so it support any Raspberry Pi version)
     unsigned peripheral_size = bcm_host_get_peripheral_size();        // Broadcom function to get peripheral size
     MAP_ADDRESS = memory_map(peripheral_address, peripheral_size);    // Map peripheral memory
 }
 
+// Check if peripherals are mapped
 void check_peripheral_map(){
     if (!MAP_ADDRESS){
         puts("Peripheral not mapped!");
@@ -113,11 +154,12 @@ void check_peripheral_map(){
     }
 }
 
+// Set gpio pin function (pin number is in BCM format)
 void set_gp_func(unsigned int pin, GP_FUNCTION function){
-    check_peripheral_map();
+    check_peripheral_map();                     // Always check if peripherals are mapped before access to them
     if (pin >= 0 && pin <= 53){
-        unsigned int reg_number = pin / FLAG_PER_REGISTER;  // calculate register number
-        unsigned int pin_number = pin % FLAG_PER_REGISTER;  // calculate pin number of this register
+        unsigned int reg_number = pin / FLAGS_PER_REGISTER;  // calculate register number
+        unsigned int pin_number = pin % FLAGS_PER_REGISTER;  // calculate pin number of this register
 
         unsigned reg[] = GPFSEL;                 // init array of pointer to GPFSEL registers addresses
         uint32_t value = GET(reg[reg_number]);   // get register value
@@ -130,49 +172,67 @@ void set_gp_func(unsigned int pin, GP_FUNCTION function){
     }
 }
 
-void set_clk_frequency(uint32_t frequency){
-    check_peripheral_map();
-    uint32_t clock_divisor = ((float)(PLLDFREQ / frequency)) * ( 1 << 12 );  // Calculate frequency clock divisor (both integer and fractional part)
+void reset_clk_generator(CM_GPCLK gpclk_number, CM_SRC clk_source){
+    check_peripheral_map();                                     // Always check if peripherals are mapped before access to them
+    unsigned reg_ctl[] = CM_CTL;                                // Init array of pointer to CM_CTL registers addresses
 
-    SET(CM_GP0CTL, 0x5A000006);     // Turn off enable flag
-    while(GET(CM_GP0CTL) & 0x80);   // Wait for busy flag to turn off
-
-    SET(CM_GP0DIV, 0x5A000000 | clock_divisor);  // Configure frequency divisor
-    SET(CM_GP0CTL, 0x5a000206);                  // Source PLLD (500MHz), 1-stage MASH (to support fractional divider)
-    SET(CM_GP0CTL, 0x5A000216);                  // Enable clock
-    while(!(GET(CM_GP0CTL) & 0x80));             // Wait for busy flag to turn on
+    while(GET(reg_ctl[gpclk_number]) & CM_BUSY){                // Unless busy flag turn off
+        SET(reg_ctl[gpclk_number], CM_PASSWD | clk_source);     // Disable clock generator
+    }
 }
 
+void start_clk_generator(CM_GPCLK gpclk_number, uint32_t clock_divisor, CM_SRC clk_source, CM_MASH mash_stage){
+    check_peripheral_map();                                             // Always check if peripherals are mapped before access to them
+    unsigned reg_ctl[] = CM_CTL;                                        // Init array of pointer to CM_CTL registers addresses
+    unsigned reg_div[] = CM_DIV;                                        // Init array of pointer to CM_DIV registers addresses
+
+    reset_clk_generator(gpclk_number, clk_source);                      // Disable clock generator before any changes
+
+    SET(reg_div[gpclk_number], CM_PASSWD | clock_divisor);              // Set frequency divisor
+    SET(reg_ctl[gpclk_number], CM_PASSWD | mash_stage | clk_source);    // Set source and MASH
+
+    while(!(GET(reg_ctl[gpclk_number]) & CM_BUSY)){                     // Unless busy flag turn on
+        SET(reg_ctl[gpclk_number], CM_PASSWD | GET(reg_ctl[gpclk_number]) | CM_ENAB);  // Enable clock generator
+    }
+}
+
+// Set clock frequency to GPCLK pin
+void set_clk_frequency(CM_GPCLK gpclk_number, uint32_t frequency){
+    uint32_t clock_divisor = ((float)(PLLDFREQ / frequency)) * (1 << 12);   // Calculate frequency clock divisor (both integer and fractional part)
+    start_clk_generator(gpclk_number, clock_divisor, CM_PLLD, CM_MASH_1S);  // Set clock_divisor, source PLLD (500MHz), 1-stage MASH (to support fractional divisor)
+}
+
+// Stop clock generator and reset GPIO4 to OUTPUT function
 void exit_handler(int signum){
-    check_peripheral_map();
     puts("Cleaning resources...");
-    set_gp_func(4, GP_OUTPUT);   // Set GPIO4 as output
-    SET(CM_GP0CTL, 0x5A000000);  // Disable the clock generator.
-    exit(signum);                // Close program
+    set_gp_func(4, GP_OUTPUT);                  // Set GPIO4 as output
+    reset_clk_generator(CM_GPCLK0, CM_PLLD);    // Disable clock generator
+    exit(signum);                               // Close program
 }
 
+// Set exit program handler
 void set_signal_handler(void (*handler)(int)){
-    if (signal(SIGQUIT, handler) == SIG_ERR)  // Set SIGQUIT signal handler to reset gpio on exit
+    if (signal(SIGQUIT, handler) == SIG_ERR)    // Set SIGQUIT signal handler to reset gpio on exit
         puts("Signal (SIGQUIT) error");
-    if (signal(SIGINT, handler) == SIG_ERR)   // Set SIGINT signal handler to reset gpio on exit
+    if (signal(SIGINT, handler) == SIG_ERR)     // Set SIGINT signal handler to reset gpio on exit
         puts("Signal (SIGINT) error");
 }
 
 int main(int argc, char **argv){
 
-    uint32_t carrier_frequency = 104500000; // Default frequency 104.5 MHz
+    uint32_t carrier_frequency = 104500000;        // Default frequency 104.5 MHz
     if (argc > 1)
-        carrier_frequency = atoi(argv[1]);  // Parse first argument as frequency if present
+        carrier_frequency = atoi(argv[1]);         // Parse first argument as frequency if present
 
-    // Call this before access peripherals
-    map_peripheral();  // Map peripheral with right addresses depending on board type
+    // Call this only once, before access peripherals
+    map_peripheral();                              // Map peripheral with right addresses depending on board type
 
-    set_gp_func(4, GP_AF0);                 // Set pin 4 as clock (Alternative Function 0)
-    set_clk_frequency(carrier_frequency);   // Set frequency and enable clock
+    set_gp_func(4, GP_AF0);                        // Set GPIO4 [pin 7] as clock (Alternative Function 0)
+    set_clk_frequency(CM_GPCLK0, carrier_frequency);  // Set the clock frequency to GPCLK0 (because we are using GPIO4 [pin 7])
 
-    set_signal_handler(exit_handler);       // Set handler to clean resources on exit signal
+    set_signal_handler(exit_handler);              // Set handler to clean resources on exit signal
     printf("Transmitting carrier on %d Hz\n", carrier_frequency);
-    while(1);                               // Run forever
+    while(1);                                      // Run forever
 
     return 0;
 }
